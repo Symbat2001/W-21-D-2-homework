1cdffee160ab92c5d8fedcb74ca49bd79f3c07c0
Symbat <tazhben01@gmail.com>
2025-01-29 00:54:40 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const area = document.getElementById("textInput")
+const saveBtn = document.getElementById("save")
+const clearBtn = document.getElementById("clear") 
+
+if (localStorage.getItem("save")) {
+    area.value = localStorage.getItem("save")
+}
+
+saveBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    localStorage.setItem("save", area.value)
+    alert("Málımet sáttı saqtaldy!")
+    
+})
+
+clearBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    localStorage.clear()
+    area.value = ""
+    alert("Mátın óshırıldı!")
+
+})
